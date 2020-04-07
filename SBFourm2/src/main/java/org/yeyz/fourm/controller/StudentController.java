319b852cfa7ddf718c8f25ab270bdcc4fb807fbc
yyz123212 <1656368645@qq.com>
2020-04-07 17:39:50 +0800

    第二次更新测试

--- SBFourm2/src/main/java/org/yeyz/fourm/controller/StudentController.java
+++ SBFourm2/src/main/java/org/yeyz/fourm/controller/StudentController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yeyz.fourm.entity.Student;
 import org.yeyz.fourm.service.IStudentService;
-
+//测试用 学生类控制器
 @Controller
 
 public class StudentController {
