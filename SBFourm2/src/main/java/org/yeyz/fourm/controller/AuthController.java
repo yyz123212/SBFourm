8c57e0755f63efe5eb7466f9ab2f52a3bcf80fb2
yyz123212 <1656368645@qq.com>
2020-05-11 10:16:29 +0800

    完整版1.23

--- SBFourm2/src/main/java/org/yeyz/fourm/controller/AuthController.java
+++ SBFourm2/src/main/java/org/yeyz/fourm/controller/AuthController.java
@@ -13,11 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.yeyz.fourm.dto.AccessTokenDTO;
 import org.yeyz.fourm.dto.GitHubUser;
